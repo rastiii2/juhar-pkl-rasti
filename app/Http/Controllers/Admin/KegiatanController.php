408ced7e9b063431d89c7923c6bef2423778eb9e
rastiii2 <rastianggraini62@smp.belajar.id>
2024-10-25 11:56:40 +0700

    Membuat dan Menampilkan Kegiatan Siswa serta Validasi untuk Guru

---
+++ app/Http/Controllers/Admin/KegiatanController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Kegiatan;
+use App\Models\Admin\Pembimbing;
+use App\Models\Admin\Siswa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class KegiatanController extends Controller
+{
+    public function kegiatan($id, $id_siswa)
+    {
+        $loginGuru = Auth::guard('guru')->user()->id_guru;
+        $siswa = Siswa::find($id_siswa);
+
+        if(!$siswa || !$siswa->id_pembimbing) {
+            return back()->withErrors(['access' => 'Siswa tidak ditemukan atau tidak memiliki pembimbing.']);
+        }
+
+        if ($siswa->id_pembimbing != $id) {
+            return back()->withErrors(['access' => 'Pembimbing tidak sesuai.']);
+        }
+
+        $pembimbing = Pembimbing::find($id);
+
+        if (!$pembimbing || $pembimbing->id_guru !== $loginGuru) {
+            return back()->withErrors(['access' => 'Akses Anda ditolak. Siswa ini tidak dibimbing oleh Anda.']);
+        }
+
+        $kegiatans = Kegiatan::where('id_siswa', $id_siswa)->get();
+        $kegiatan = Kegiatan::where('id_siswa', $id_siswa)->first();
+        $id_pembimbing = $id;
+
+        return view('guru.kegiatan', compact('id_pembimbing','kegiatans', 'kegiatan'));
+    }
+
+    public function detailKegiatan() 
+    {
+    }
+    
+}
---
+++ app/Models/Admin/Kegiatan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kegiatan extends Model
+{
+    use HasFactory;
+    protected $table = 'kegiatan';
+    protected $primaryKey = 'id_kegiatan';
+
+    protected $fillable = [
+        'id_siswa',
+        'tanggal_kegiatan',
+        'nama_kegiatan',
+        'ringkasan_kegiatan',
+        'foto',
+    ];
+
+    public function kegiatanSiswa()
+    {
+        return $this->belongsTo(Siswa::class, 'id_siswa', 'id_siswa');
+    }
+}
+
